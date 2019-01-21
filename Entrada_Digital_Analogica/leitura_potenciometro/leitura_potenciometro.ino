12205db9978bfd7a3590b6d973b706222f333dc4
virginiasatyro <vivi_satyro@hotmail.com>
2019-01-21 13:24:04 -0200

    reorganizando o código básico

--- Entrada_Digital_Analogica/leitura_potenciometro/leitura_potenciometro.ino
+++ Entrada_Digital_Analogica/leitura_potenciometro/leitura_potenciometro.ino
@@ -1,3 +1,14 @@
+/***********************************************************************************
+ * File: leitura_potenciometro
+ * Arduino UNO
+ * Arduino IDE
+ * Author: Virgínia Sátyro
+ * License: Free - Open Source
+ * Created on Janeiro de 2019
+ * 
+ * Leitura de um potenciômetro de 10k através da entrada analógica A0. 
+ * O Arduino tem leitura analógica de 10 bits (0 a 1023).
+***********************************************************************************/
 const int pot = A0; // potenciômetro no pino A0
 
 void setup() {
@@ -9,7 +20,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int leitura = analogRead(pot); // lê o valor do potenciômetro
-  Serial.println(leitura);
+  Serial.println(leitura);       // valor entre 0 e 1023
   
   delay(1); // delay entre leituras - estabilidade
 }
