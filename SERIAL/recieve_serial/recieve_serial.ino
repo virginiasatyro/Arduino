920fbaa993e3d498261169283eaa8ee014e22612
virginiasatyro <vivi_satyro@hotmail.com>
2019-01-22 19:37:09 -0200

    atualização SPI e SD

---
+++ SERIAL/recieve_serial/recieve_serial.ino
@@ -0,0 +1,31 @@
+/***********************************************************************************
+ * File: recieve_serial
+ * Arduino UNO
+ * Arduino IDE
+ * Author: Virgínia Sátyro
+ * License: Free - Open Source
+ * Created on Janeiro de 2019
+ * 
+ * Escrever um comando no terminal serial e verificar qual string foi recebida.
+ * É precisso corrigir uns bugs!!
+***********************************************************************************/
+
+String msg; 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()){
+    msg = Serial.readString();
+    Serial.println(msg);
+  }
+
+  if(msg == "um")  { Serial.println("Você digitou um.");}
+  if(msg == "dois"){ Serial.println("Você digitou dois.");}
+  if(msg == "tres"){ Serial.println("Você digitou tres.");}
+  else             { Serial.println("Mensagem não identificada!");}
+  delay(400);
+}
---
+++ SPI/SD/record_SD/record_SD.ino
@@ -0,0 +1,44 @@
+/***********************************************************************************
+ * File: record_SD
+ * Arduino UNO
+ * Arduino IDE
+ * Author: Virgínia Sátyro
+ * License: Free - Open Source
+ * Created on Janeiro de 2019
+ * 
+ * O objetivo desse projeto é fazer o primeiro teste do cartão SD, ou seja, abrir
+ * um arquivo, gravar uma string e fechar.
+***********************************************************************************/
+#include<SdFat.h>
+
+SdFat sdCard;
+SdFile file;
+
+const int ss = 10;   // select-slave
+bool record_flag = 0; // if flag is 0 - file not recorded
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  // initializing SD module
+  if(!sdCard.begin(ss, SPI_HALF_SPEED))sdCard.initErrorHalt(); 
+  if(!file.open("file_recorded.txt", O_RDWR | O_CREAT | O_AT_END)){
+    sdCard.errorHalt("Error opening file!");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(record_flag == 0){
+    Serial.println("Recording file, please wait...");
+    for(int i = 0; i < 100; i++){
+      file.print(i);
+      file.println(" TC is the best!");
+      delay(1);
+     }
+
+    Serial.println("File recorded, you can remove SD card now!");
+    file.close(); // close the file
+    record_flag = 1; // file recorded, avoid recording again the same information 
+  }
+}
